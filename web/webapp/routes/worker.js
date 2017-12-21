0327a1e409e83d0360ab5c2ef24a8c0ef7c6dcc0
Anton Kojin <antonkojin@gmail.com>
2017-12-21 21:20:59 +0100

    worker's web endpoints

--- web/webapp/routes/worker.js
+++ web/webapp/routes/worker.js
@@ -9,4 +9,29 @@ router.get('/login', function(req, res, next) {
     res.render('login', {title: 'Worker login'});
 });
 
+router.get('/campaigns', function(req, res, next) {
+    //TODO
+    res.send('my campaigns and applyable campaigns');
+});
+
+router.get('/campaign/apply/:campaignId', function(req, res, next) {
+    // res.send(req.params);
+    //TODO
+    res.send('applyed for a new campaign');
+});
+
+router.get('/campaign/:campaignId/task', function(req, res, next) {
+    //TODO
+    res.send('execute task (create new one, if necessary)');
+});
+
+let getTasksAnd = (fun) => {
+    require('fs').readFile(
+        require('path').join(__dirname, '../public/esempio_task.json'),
+        'utf8',
+        (err, json) => fun(JSON.parse(json))
+    );
+};
+
+
 module.exports = router;
