2d08cae67694a45deeb843f69e9dda549af833b1
Anton Kojin <antonkojin@gmail.com>
2018-03-26 17:56:38 +0200

    db init problem

--- db/init_user_db.sql
+++ db/init_user_db.sql
@@ -2,3 +2,9 @@ CREATE USER db_user;
 CREATE DATABASE db_name;
 GRANT ALL PRIVILEGES ON DATABASE db_name TO db_user;
 
+CREATE TABLE db_name.users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    type CHAR(1) -- (worker, requester)
+);
