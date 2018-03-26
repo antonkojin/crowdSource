7022dea16f4376277cbad0c02d34e31ac74aa06d
Anton Kojin <antonkojin@gmail.com>
2018-03-26 17:04:04 +0200

    no nexxt in router funs when unnecessary

--- web/webapp/routes/login.js
+++ web/webapp/routes/login.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var db = require('../lib/db');
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('login');
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
   db.testQuery();
   db.login(req.body.email, req.body.password,
     () => res.send('logged in'),
