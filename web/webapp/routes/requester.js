cf25d93487dbf3bc7e9b52d67bef4629c966de80
Anton Kojin <antonkojin@gmail.com>
2018-04-10 17:36:11 +0200

    message

--- web/webapp/routes/requester.js
+++ web/webapp/routes/requester.js
@@ -1,20 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/db');
+var { inspect } = require('util');
 
-router.get('/verification', function(req, res, next) {
-    res.render('requester-verification');
+router.get('/verification', function (req, res, next) {
+  res.render('requester-verification');
 });
 
-router.get('/new-campaign', function(req, res, next) {
-    res.render('requester-campaign-creation');
+router.get('/new-campaign', function (req, res, next) {
+  res.render('requester-campaign-creation');
 });
 
-router.get('/:campaignId/report', function(req, res, next) {
-    res.render('requester-campaign-report', {campaignId: req.params.campaignId});
+router.post('/new-campaign', function (req, res) {
+  // TODO
+  console.log(inspect(req.body));
+  const query = 'SELECT * FROM "user" WHERE email = ${email};';
+  const parameters = {email: 'wrong email'};
+  db.db.none(query, parameters)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(error => {
+      console.error(error);
+      res.sendStatus(500);
+    });
 });
 
-router.get('/campaigns', function(req, res, next) {
-    res.render('requester-campaigns');
+router.get('/:campaignId/report', function (req, res, next) {
+  res.render('requester-campaign-report', { campaignId: req.params.campaignId });
+});
+
+router.get('/campaigns', function (req, res, next) {
+  res.render('requester-campaigns');
 });
 
 module.exports = router;
